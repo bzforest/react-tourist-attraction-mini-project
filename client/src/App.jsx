6cd70e5cb8aad94ea13bfd9523eea0d95abd7c49
Best <bestsakditat@gmail.com>
2026-01-16 09:49:05 +0700

    chore: update dependencies and configure Tailwind CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
+{/* 
+1. Server Connection:
+ต้องรัน Server (Port 4001) ให้ติดก่อน
+API Endpoint: GET http://localhost:4001/trips?keywords=xxx
+
+2. Search Feature:
+พิมพ์ปุ๊บ ค้นหาปั๊บ (หรืออาจจะต้องกด Enter แล้วแต่ดีไซน์ แต่โจทย์บอก "พิมพ์คำค้นหา... เพื่อค้นหา"
+
+3. UI Display:
+แสดงรายการที่ค้นหาเจอ
+รูปภาพ: รูปใหญ่ 1 รูป (ตัวแรกใน array), รูปเล็ก 3 รูป
+Text: Title, Description (ตัดคำไม่เกิน 100 ตัวอักษร!)
+Links: คลิก Title หรือ "อ่านต่อ" ต้องเปิด Tab ใหม่
+Tags: แสดงหมวดหมู่
+
+*/}
+
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return (
+  <div className="App">
+    
+  </div>
+  )
 }
 
 export default App;
